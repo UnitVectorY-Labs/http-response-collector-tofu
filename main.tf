837530310893a0df747bfc0cfffe4d2a7b19d517
Jared Hatfield <jjhatf02@gmail.com>
2025-02-03 18:58:14 -0500

    Add Eventarc and Pub/Sub resources for Cloud Run integration

--- main.tf
+++ main.tf
@@ -5,6 +5,12 @@ resource "google_project_service" "run" {
   disable_on_destroy = false
 }
 
+resource "google_project_service" "eventarc" {
+  project            = var.project_id
+  service            = "eventarc.googleapis.com"
+  disable_on_destroy = false
+}
+
 resource "google_project_service" "pubsub" {
   project            = var.project_id
   service            = "pubsub.googleapis.com"
@@ -16,6 +22,12 @@ resource "google_service_account" "cloud_run_sa" {
   project      = var.project_id
   account_id   = "${var.name}-sa"
   display_name = "${var.name} Cloud Run Service Account"
+}
+
+# Pub/Sub topic to receive messages and forward to Cloud Run services
+resource "google_pubsub_topic" "request" {
+  project = var.project_id
+  name    = "${var.name}-request"
 }
 
 locals {
@@ -51,4 +63,40 @@ resource "google_cloud_run_v2_service" "http_response_collector" {
   depends_on = [
     // TODO
   ]
+}
+
+# Service account for Eventarc triggers
+resource "google_service_account" "eventarc_sa" {
+  project      = var.project_id
+  account_id   = "${var.name}-eventarc-sa"
+  display_name = "${var.name} Eventarc Service Account"
+}
+
+# IAM role to grant invoke permissions to Eventarc service account for Cloud Run services
+resource "google_cloud_run_service_iam_member" "invoke_permission" {
+  project  = var.project_id
+  location = var.region
+  service  = google_cloud_run_v2_service.http_response_collector.name
+  role     = "roles/run.invoker"
+  member   = "serviceAccount:${google_service_account.eventarc_sa.email}"
+}
+
+# Pub/Sub subscription to forward messages to Cloud Run services
+resource "google_pubsub_subscription" "pubsub_subscription" {
+  project                 = var.project_id
+  name                    = "bqpas-${var.name}-${var.region}"
+  topic                   = google_pubsub_topic.request.name
+  enable_message_ordering = true
+
+  push_config {
+    push_endpoint = "${google_cloud_run_v2_service.http_response_collector.uri}/pubsub/push"
+
+    oidc_token {
+      service_account_email = google_service_account.eventarc_sa.email
+    }
+
+    attributes = {
+      x-goog-version = "v1"
+    }
+  }
 }
